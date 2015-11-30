9ea198a13f05ebfbccb50583dba99a1fd7959908
sbjohnson <sbjohnson@quinnipiac.edu>
2015-11-30 00:40:10 +0000

    Add Review create view

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from.views import *
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),   
     url(r'^user/', include('registration.backends.simple.urls')),
-    url(r'^user/', include('django.contrib.auth.urls')),                   
+    url(r'^user/', include('django.contrib.auth.urls')),
+    url(r'^review/create/$', ReviewCreateView.as_view(), name='review_create'),
+                       
 )
