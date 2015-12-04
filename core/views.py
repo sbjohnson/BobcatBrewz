52401118c5c57baf311d7045b098b6e6867054b6
sbjohnson <sbjohnson@quinnipiac.edu>
2015-12-04 14:31:52 +0000

    Add user authorization

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
+from django.core.exceptions import PermissionDenied
 from .models import *
 
 # Create your views here.
@@ -37,10 +38,22 @@ class ReviewUpdateView(UpdateView):
     template_name = 'review/review_form.html'
     fields = ['Beer', 'description']
 
+    def get_object(self, *args, **kwargs):
+        object = super(ReviewUpdateView, self).get_object(*args, **kwargs)
+        if object.user != self.request.user:
+            raise PermissionDenied()
+        return object
+
 class ReviewDeleteView(DeleteView):
     model = Review
     template_name = 'review/review_confirm_delete.html'
     success_url = reverse_lazy('review_list')
+
+    def get_object(self, *args, **kwargs):
+        object = super(ReviewDeleteView, self).get_object(*args, **kwargs)
+        if object.user != self.request.user:
+            raise PermissionDenied()
+        return object
 
 class CommentCreateView(CreateView):
     model = Comment
@@ -64,6 +77,12 @@ class CommentUpdateView(UpdateView):
     def get_success_url(self):
         return self.object.review.get_absolute_url()
 
+    def get_object(self, *args, **kwargs):
+        object = super(CommentpdateView, self).get_object(*args, **kwargs)
+        if object.user != self.request.user:
+            raise PermissionDenied()
+        return object
+
 class CommentDeleteView(DeleteView):
     model = Comment
     pk_url_kwarg = 'comment_pk'
@@ -71,3 +90,9 @@ class CommentDeleteView(DeleteView):
 
     def get_success_url(self):
         return self.object.review.get_absolute_url()
+
+    def get_object(self, *args, **kwargs):
+        object = super(CommentDeleteView, self).get_object(*args, **kwargs)
+        if object.user != self.request.user:
+            raise PermissionDenied()
+        return object
